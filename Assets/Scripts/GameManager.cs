da40b58c1dff1bdbbcbcfb79cd4d44f8d99d635f
Mikael <50752113+MikaelVV@users.noreply.github.com>
2021-06-29 20:33:17 +0300

    Score counterin aloittelua

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Text scoreText;
+
+    int score = 0;
+
+    void Start()
+    {
+        scoreText.text = score.ToString() + "Score";
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
