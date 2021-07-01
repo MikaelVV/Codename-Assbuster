5eb738e79fa0ab66c95819b453612417a7fbbd55
Mikael <50752113+MikaelVV@users.noreply.github.com>
2021-07-01 16:20:09 +0300

    Kauppa melkein valmis

---
+++ Assets/Scripts/ShopButtonInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopButtonInfo : MonoBehaviour
+{
+
+    public int ItemID;
+    public Text priceText;
+    public Text Quantitytext;
+    public GameObject ShopManager;
+
+
+    void Update()
+    {
+        priceText.text = "" + ShopManager.GetComponent<ShopManager>().shopItems[2, ItemID].ToString();
+        Quantitytext.text = ShopManager.GetComponent<ShopManager>().shopItems[3, ItemID].ToString();
+    }
+}
---
+++ Assets/Scripts/ShopManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ShopManager : MonoBehaviour
+{
+
+    public int[,] shopItems = new int[11,11];
+    public float shopTokens;
+    public Text ShopTokentext;
+
+
+
+    void Start()
+    {
+        ShopTokentext.text = "$" + shopTokens.ToString();
+
+        //Kaupassa ostettavien esineiden IDt
+        shopItems[1, 1] = 1;
+        shopItems[1, 2] = 2;
+        shopItems[1, 3] = 3;
+        shopItems[1, 4] = 4;
+        shopItems[1, 5] = 5;
+        shopItems[1, 6] = 6;
+        shopItems[1, 7] = 7;
+        shopItems[1, 8] = 8;
+        shopItems[1, 9] = 9;
+        shopItems[1, 10] = 10;
+        shopItems[1, 11] = 11;
+
+        //Hinnat
+        shopItems[2, 1] = 1;
+        shopItems[2, 2] = 2;
+        shopItems[2, 3] = 3;
+        shopItems[2, 4] = 4;
+        shopItems[2, 5] = 5;
+        shopItems[2, 6] = 6;
+        shopItems[2, 7] = 7;
+        shopItems[2, 8] = 8;
+        shopItems[2, 9] = 9;
+        shopItems[2, 10] = 10;
+        shopItems[2, 11] = 11;
+
+        //Määrä
+        shopItems[3, 1] = 0;
+        shopItems[3, 2] = 0;
+        shopItems[3, 3] = 0;
+        shopItems[3, 4] = 0;
+        shopItems[3, 5] = 0;
+        shopItems[3, 6] = 0;
+        shopItems[3, 7] = 0;
+        shopItems[3, 8] = 0;
+        shopItems[3, 9] = 0;
+        shopItems[3, 10] = 0;
+        shopItems[3, 11] = 0;
+    }
+
+
+    public void BuyItems()
+    {
+        GameObject ButtonRef = GameObject.FindGameObjectWithTag("Event").GetComponent<EventSystem>().currentSelectedGameObject;
+
+        if(shopTokens >= shopItems[2, ButtonRef.GetComponent<ShopButtonInfo>().ItemID])
+        {
+            shopTokens -= shopItems[2, ButtonRef.GetComponent<ShopButtonInfo>().ItemID];
+            shopItems[3, ButtonRef.GetComponent<ShopButtonInfo>().ItemID]++;
+            ShopTokentext.text = "" + shopTokens.ToString();
+            ButtonRef.GetComponent<ShopButtonInfo>().Quantitytext.text = shopItems[3, ButtonRef.GetComponent<ShopButtonInfo>().ItemID].ToString();
+        }
+    }
+}
