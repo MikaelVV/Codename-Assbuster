dd55e3c5fd210b1042e470a2f60c187b8fe6c34a
Mikael <50752113+MikaelVV@users.noreply.github.com>
2021-09-19 21:51:52 +0300

    Päivitystä LevelSelectin asevalikkoon ja sen toimintoihin. Katso description, jos haluat lisätietoa
    Tein scriptableobjects instanssin aseista, joiden avulla levelselectissä valitut aseet saadaan toimimaan pelissä.

---
+++ Assets/Scripts/GunScripts/GunItem.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Guns", menuName = "ScriptableObjects/gunManagerScriptableObjects", order = 1)]
+public class GunItem : ScriptableObject
+{
+    public GameObject Prefab;
+
+}
